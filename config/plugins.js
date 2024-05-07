d4d2cd7aba2831c40cb11c4a6cd4090db9d569f4
codingschool_tmatusek <tomasmatusek@seznam.cz>
2024-05-07 06:41:28 +0200

    Added nodemailer provider

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,22 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  // ...
+  email: {
+    config: {
+      provider: "nodemailer",
+      providerOptions: {
+        host: env("SMTP_HOST", "smtp.example.com"),
+        port: env("SMTP_PORT", 587),
+        auth: {
+          user: env("SMTP_USERNAME"),
+          pass: env("SMTP_PASSWORD"),
+        },
+        // ... any custom nodemailer options
+      },
+      settings: {
+        defaultFrom: "misage87@seznam.cz",
+        defaultReplyTo: "tomasmatusek@seznam.cz",
+      },
+    },
+  },
+  // ...
+});
